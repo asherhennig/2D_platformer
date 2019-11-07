8ba6fd069796aac14f15a2b024c8969e79e160f8
Asher Hennig [Student] <asher.hennig@student.sl.on.ca>
2019-11-07 11:53:34 -0500

    added animations and transitions

--- SuperSoyBoy/Assets/Scripts/SoyBoyController.cs
+++ SuperSoyBoy/Assets/Scripts/SoyBoyController.cs
@@ -131,6 +131,8 @@ public class SoyBoyController : MonoBehaviour
         //1
         input.x = Input.GetAxis("Horizontal");
         input.y = Input.GetAxis("Jump");
+
+        animator.SetFloat("Speed", Mathf.Abs(input.x));
         //2
         if (input.x > 0f)
         {
@@ -147,6 +149,7 @@ public class SoyBoyController : MonoBehaviour
             {
                 isJumping = true;
             }
+            animator.SetBool("IsOnWall", false);
         }
 
         if (jumpDuration > jumpDurationThreshold) input.y = 0f;
@@ -154,10 +157,12 @@ public class SoyBoyController : MonoBehaviour
         if (input.y >= 1f)
         {
             jumpDuration += Time.deltaTime;
+            animator.SetBool("IsJumping", true);
         }
         else
         {
             isJumping = false;
+            animator.SetBool("IsJumping", false);
             jumpDuration = 0f;
         }
     }
@@ -201,11 +206,21 @@ public class SoyBoyController : MonoBehaviour
         //4
         rb.velocity = new Vector2(xVelocity, yVelocity);
 
-        if (IsWallToLeftOrRight() && !PlayerIsOnGround()
-            && input.y == 1)
+        if (IsWallToLeftOrRight() && !PlayerIsOnGround() && input.y == 1)
         {
-            rb.velocity = new Vector2(-GetWallDirection()
-                * speed * 0.75f, rb.velocity.y);
+            rb.velocity = new Vector2(-GetWallDirection() * speed 
+                * 0.75f, rb.velocity.y);
+            animator.SetBool("IsOnWall", false);
+            animator.SetBool("IsJumping", true);
+        }
+        else if (!IsWallToLeftOrRight())
+        {
+            animator.SetBool("IsOnWall", false);
+            animator.SetBool("IsJumping", true);
+        }
+        if (IsWallToLeftOrRight() && !PlayerIsOnGround())
+        {
+            animator.SetBool("IsOnWall", true);
         }
 
         if (isJumping && jumpDuration < jumpDurationThreshold)
