7861b0f5ac3e327b8b862f9a612983df1d012e24
asher hennig <asherhennig@gmail.com>
2019-11-19 21:30:06 -0500

    added timer

--- SuperSoyBoy/Assets/Scripts/GameManager.cs
+++ SuperSoyBoy/Assets/Scripts/GameManager.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 
 public class GameManager : MonoBehaviour
 {
@@ -36,12 +41,80 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public List<PlayerTimeEntry> LoadPreviousTime()
+    {
+        //1
+        try
+        {
+            var scoresFile = Application.persistentDataPath +
+                "/" + playerName + "_times.dat";
+            using (var stream = File.Open(scoresFile, FileMode.Open))
+            {
+                var bin = new BinaryFormatter();
+                var times = (List<PlayerTimeEntry>)bin.Deserialize(stream);
+                return times;
+            }
+        }
+        //2
+        catch (IOException ex)
+        {
+            Debug.LogWarning("Couldn't load previous time for: " +
+                playerName + ". Exception: " + ex.Message);
+            return new List<PlayerTimeEntry>();
+        }
+    }
+
+    public void SaveTime(decimal time)
+    {
+        //3
+        var times = LoadPreviousTime();
+        //4
+        var newTime = new PlayerTimeEntry();
+        newTime.entryDate = DateTime.Now;
+        newTime.time = time;
+        //5
+        var bFormatter = new BinaryFormatter();
+        var filePath = Application.persistentDataPath +
+            "/" + playerName + "_times.dat";
+        using (var file = File.Open(filePath, FileMode.Create))
+        {
+            times.Add(newTime);
+            bFormatter.Serialize(file, times);
+        }
+    }
+
+    public void DisplayPreviousTimes()
+    {
+        //1
+        var times = LoadPreviousTime();
+        var topThree = times.OrderBy(time => time.time).Take(3);
+        //2
+        var timesLabel = GameObject.Find("PreviousTimes")
+            .GetComponent<Text>();
+
+        //3
+        timesLabel.text = "BEST TIMES \n";
+        foreach (var time in topThree)
+        {
+            timesLabel.text += time.entryDate.ToShortDateString() +
+                ": " + time.time + "\n";
+        }
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode loadsceneMode)
+    {
+        if (scene.name == "Game")
+        {
+            DisplayPreviousTimes();
+        }
     }
 }
--- SuperSoyBoy/Assets/Scripts/Goal.cs
+++ SuperSoyBoy/Assets/Scripts/Goal.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class Goal : MonoBehaviour
 {
     public AudioClip goalClip;
+
     void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.gameObject.tag == "Player")
         {
             var audioSource = GetComponent<AudioSource>();
+
             if (audioSource != null && goalClip != null)
             {
                 audioSource.PlayOneShot(goalClip);
             }
             GameManager.instance.RestartLevel(0.5f);
+
+            //1
+            var timer = FindObjectOfType<Timer>();
+            //2
+            GameManager.instance.SaveTime(timer.time);
         }
     }
  
